99fb82a6522b98b98ae645f47f49368a0766f2f4
Kenny Hung <khung@instructors.2u.com>
2022-02-19 13:21:05 +0000

    Working skeletal script

---
+++ generate.js
@@ -0,0 +1,11 @@
+// Inquirer all the stuff
+// Read the template
+// Fill out the template with the stuff
+// Write the templated file to index.html
+const inquirer = require('inquirer');
+
+async function generate() {
+    console.log('Hello')
+}
+
+generate()
