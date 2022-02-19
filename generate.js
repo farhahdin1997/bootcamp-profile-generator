4b8f55454be22bb1ac9932f6df83e7970fd5750e
Kenny Hung <khung@instructors.2u.com>
2022-02-19 13:26:54 +0000

    Inquirer working

--- generate.js
+++ generate.js
@@ -1,11 +1,34 @@
-// Inquirer all the stuff
 // Read the template
 // Fill out the template with the stuff
 // Write the templated file to index.html
 const inquirer = require('inquirer');
 
 async function generate() {
-    console.log('Hello')
+    /*
+
+<p>{{name}}</p>
+<p>{{location}}</p>
+<p>{{bio}}</p>
+<p>{{github}}</p>
+*/
+    const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name, bruv?'
+    }, {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you, dude?'
+    }, {
+        type: 'input',
+        name: 'bio',
+        message: 'Tell me all about you, please.'
+    }, {
+        type: 'input',
+        name: 'github',
+        message: 'Where\'s the code at?'
+    }])
+    console.log(answers)
 }
 
 generate()
